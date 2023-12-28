3407331f60952a4e2aba655e8a8951e9b23ba742
Agnese Bussone <agnese.bussone@gmail.com>
2023-12-28 15:06:23 +0100

    Test that the cache is not deleted upon creation

---
+++ EssentialFeedTests/Cache/CacheTests.swift
@@ -0,0 +1,23 @@
+//
+
+import XCTest
+
+class LocalFeedLoader {
+    init(store: FeedStore) {
+        
+    }
+}
+
+class FeedStore {
+    var deleteCachedFeedCallCount = 0
+}
+
+class CacheTests: XCTestCase {
+    
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+}
