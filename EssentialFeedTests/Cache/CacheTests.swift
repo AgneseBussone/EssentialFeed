0febb39e232b5321f93d4ef44af659861d046fdd
Agnese Bussone <agnese.bussone@gmail.com>
2023-12-29 11:02:14 +0100

    Add an alias for return type

--- EssentialFeedTests/Cache/CacheTests.swift
+++ EssentialFeedTests/Cache/CacheTests.swift
@@ -74,7 +74,7 @@ class CacheTests: XCTestCase {
         let store = FeedStoreSpy()
         var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
         
-        var receivedResults = [Error?]()
+        var receivedResults = [LocalFeedLoader.SaveResult]()
         sut?.save([uniqueItem()], completion: { receivedResults.append($0) })
         
         sut = nil
@@ -87,7 +87,7 @@ class CacheTests: XCTestCase {
         let store = FeedStoreSpy()
         var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
         
-        var receivedResults = [Error?]()
+        var receivedResults = [LocalFeedLoader.SaveResult]()
         sut?.save([uniqueItem()], completion: { receivedResults.append($0) })
         
         store.completeDeletionSuccessfully()
