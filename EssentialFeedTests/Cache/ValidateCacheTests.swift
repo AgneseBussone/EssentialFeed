ae60a2ba5f725df693d278ee629fb597912a0f68
Agnese Bussone <agnese.bussone@gmail.com>
2024-01-02 16:40:00 +0100

    Validate command doesn't deliver data if instance has been deallocated

--- EssentialFeedTests/Cache/ValidateCacheTests.swift
+++ EssentialFeedTests/Cache/ValidateCacheTests.swift
@@ -65,6 +65,19 @@ final class ValidateCacheTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCacheFeed])
     }
     
+    func test_validateCache_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
+        let store = FeedStoreSpy()
+        var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
+        
+        var receivedResults = [LocalFeedLoader.LoadResult]()
+        sut?.validateCache()
+        
+        sut = nil
+        store.completeRetrieval(with: anyNSError())
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
