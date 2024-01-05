f1d670a52b456cf2beb90ca841c8b4521e09f930
Agnese Bussone <agnese.bussone@gmail.com>
2024-01-05 15:29:43 +0100

    Create FeedStoreSpec that must be implemented by any FeedStore test implementation to guarantee it meets the spec

--- EssentialFeedTests/Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Cache/CodableFeedStoreTests.swift
@@ -3,8 +3,7 @@
 import XCTest
 import EssentialFeed
 
-final class CodableFeedStoreTests: XCTestCase {
-    
+final class CodableFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
     override func tearDown() {
         super.tearDown()
         cleanStore()
