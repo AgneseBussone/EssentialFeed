9ca0ab9a284b9c8d960fcc160b13850054961143
Agnese Bussone <agnese.bussone@gmail.com>
2024-01-08 11:14:13 +0100

    Move CoreDataFeedStore to production code

---
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -0,0 +1,22 @@
+// Use Core Data to store feed data.
+
+import Foundation
+
+public class CoreDataFeedStore: FeedStore {
+    
+    public init() {}
+    
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        
+    }
+    
+    public func insert(_ items: [EssentialFeed.LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        
+    }
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.empty)
+    }
+    
+    
+}
--- EssentialFeedTests/Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Cache/CoreDataFeedStoreTests.swift
@@ -3,22 +3,6 @@
 import XCTest
 import EssentialFeed
 
-class CoreDataFeedStore: FeedStore {
-    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        
-    }
-    
-    func insert(_ items: [EssentialFeed.LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        
-    }
-    
-    func retrieve(completion: @escaping RetrievalCompletion) {
-        completion(.empty)
-    }
-    
-    
-}
-
 final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
