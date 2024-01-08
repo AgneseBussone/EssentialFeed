a766a99c237d814ff4f63d2f0e34e70d3b4fdc85
Agnese Bussone <agnese.bussone@gmail.com>
2024-01-08 13:41:01 +0100

    Make storeURL an explicit dependency so it can be injected (tests and production won't share state)

--- EssentialFeedTests/Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Cache/CoreDataFeedStoreTests.swift
@@ -60,8 +60,17 @@ final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
+        // To load the Core Data store in CoreDataFeedStoreTests, which is part of the test target, we need to locate the Core Data data model in the bundle.
+        // Since the data model file lives in the main production bundle which is different than the test bundle it’s essential that we choose the correct bundle to load the model from.
+        // Production will use the default value.
         let storeBundle = Bundle(for: CoreDataFeedStore.self)
-        let sut = try! CoreDataFeedStore(bundle: storeBundle)
+        
+        // By using a file URL of /dev/null for the persistent store, the Core Data stack will not save SQLite artifacts to disk, doing the work in memory.
+        // This means that this option is faster when running tests, as opposed to performing I/O and actually writing/reading from disk.
+        // Moreover, when operating in-memory, you prevent cross test side-effects since this process doesn’t create any artifacts.
+        let storeURL = URL(fileURLWithPath: "/dev/null")
+        
+        let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
