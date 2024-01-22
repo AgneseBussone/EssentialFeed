05b8b13b5a14195bef65c0a68b7d9a804f8dda06
Agnese Bussone <agnese.bussone@gmail.com>
2024-01-22 16:18:11 +0100

    Extract code for refreshing in its own ViewController

---
+++ EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -0,0 +1,31 @@
+//
+
+import UIKit
+import EssentialFeed
+
+final class FeedRefreshViewController: NSObject {
+    
+    private let feedLoader: FeedLoader
+    
+    var onRefresh: (([FeedImage]) -> Void)?
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    private(set) lazy var view: UIRefreshControl = {
+        let view = UIRefreshControl()
+        view.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        return view
+    }()
+    
+    @objc func refresh() {
+        view.beginRefreshing()
+        feedLoader.load { [weak self] result in
+            if let feed = try? result.get() {
+                self?.onRefresh?(feed)
+            }
+            self?.view.endRefreshing()
+        }
+    }
+}
