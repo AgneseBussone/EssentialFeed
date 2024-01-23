36a6d094f37c6768a8b16e5e80562842a2ee3ed0
Agnese Bussone <agnese.bussone@gmail.com>
2024-01-23 10:56:05 +0100

    Extract dependency creation and composition logic from FeedViewController into a separate ViewController

--- EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -43,7 +43,7 @@ final class FeedImageCellController {
         task = imageLoader.loadImageData(from: model.url, completion: { _ in })
     }
     
-    deinit {
+    func cancelLoad() {
         task?.cancel()
     }
 }
