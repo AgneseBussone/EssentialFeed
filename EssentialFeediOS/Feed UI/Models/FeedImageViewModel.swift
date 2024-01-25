3c812cff9ff8b94c86672e821243a271de4d1496
Agnese Bussone <agnese.bussone@gmail.com>
2024-01-25 10:29:22 +0100

    Move FeedImage value translations (including loading state) to FeedImageViewModel

---
+++ EssentialFeediOS/Feed UI/Models/FeedImageViewModel.swift
@@ -0,0 +1,56 @@
+//
+
+import Foundation
+import EssentialFeed
+import UIKit
+
+final class FeedImageViewModel {
+    typealias Observer<T> = (T) -> Void
+    
+    private let imageLoader: FeedImageDataLoader
+    private let model: FeedImage
+    private var task: FeedImageDataLoaderTask?
+    
+    var onImageLoad: Observer<UIImage>?
+    var onImageLoadingStateChange: Observer<Bool>?
+    var onShouldRetryImageLoadStateChange: Observer<Bool>?
+    
+    var hasLocation: Bool {
+        return model.location != nil
+    }
+    
+    var location: String? {
+        return model.location
+    }
+    
+    var description: String? {
+        return model.description
+    }
+
+    init( model: FeedImage, imageLoader: FeedImageDataLoader) {
+        self.imageLoader = imageLoader
+        self.model = model
+    }
+    
+    func loadImageData() {
+        onImageLoadingStateChange?(true)
+        onShouldRetryImageLoadStateChange?(false)
+        task = imageLoader.loadImageData(from: model.url, completion: { [weak self] result in
+            self?.handle(result)
+        })
+    }
+    
+    private func handle(_ result: FeedImageDataLoader.Result) {
+        if let image = (try? result.get()).flatMap(UIImage.init) {
+            onImageLoad?(image)
+        } else {
+            onShouldRetryImageLoadStateChange?(true)
+        }
+        onImageLoadingStateChange?(false)
+    }
+    
+    func cancelImageDataLoad() {
+        task?.cancel()
+        task = nil
+    }
+}
