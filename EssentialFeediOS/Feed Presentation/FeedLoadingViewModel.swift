ec6e5dfc6304b04af1b63f9f096e35c2f8be2442
Agnese Bussone <agnese.bussone@gmail.com>
2024-01-29 13:28:47 +0100

    Separate View Models into different files

---
+++ EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,7 @@
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,8 @@
+//
+
+import Foundation
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
