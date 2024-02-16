5777d061d8235c9e7508aaa58ffe717b350ef734
Agnese Bussone <agnese.bussone@gmail.com>
2024-02-16 10:28:46 +0100

    Move RemoteFeedImageDataLoader to production

--- EssentialFeedTests/API/RemoteFeedImageLoaderTests.swift
+++ EssentialFeedTests/API/RemoteFeedImageLoaderTests.swift
@@ -3,60 +3,6 @@
 import XCTest
 import EssentialFeed
 
-class RemoteFeedImageDataLoader {
-    private let client: HTTPClient
-    
-    public enum Error: Swift.Error {
-        case invalidData
-    }
-    
-    init(client: HTTPClient) {
-        self.client = client
-    }
-    
-    private final class HTTPTaskWrapper: FeedImageDataLoaderTask {
-        var wrapped: HTTPClientTask?
-        private var completion: ((FeedImageDataLoader.Result) -> Void)?
-        
-        init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
-            self.completion = completion
-        }
-        
-        func complete(with result: FeedImageDataLoader.Result) {
-            completion?(result)
-        }
-        
-        func cancel() {
-            preventFurtherCompletions()
-            wrapped?.cancel()
-        }
-        
-        private func preventFurtherCompletions() {
-            completion = nil
-        }
-    }
-    
-    @discardableResult
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        let task = HTTPTaskWrapper(completion)
-        task.wrapped = client.get(from: url) { [weak self] result in
-            guard self != nil else { return }
-            
-            switch result {
-            case let .success((data, httpResponse)):
-                if httpResponse.statusCode == 200 && !data.isEmpty {
-                    task.complete(with: .success(data))
-                } else {
-                    task.complete(with: .failure(Error.invalidData))
-                }
-            case let .failure(error): task.complete(with: .failure(error))
-            }
-        }
-        return task
-    }
-}
-
-
 final class RemoteFeedImageLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
