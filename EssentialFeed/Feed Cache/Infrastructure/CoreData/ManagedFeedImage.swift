682ed6a53101b9a726e4b0faf6a20bb61daeadf8
Agnese Bussone <agnese.bussone@gmail.com>
2024-02-20 14:52:39 +0100

    Add optional Data property to the CoreData model

--- EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -8,6 +8,7 @@ internal class ManagedFeedImage: NSManagedObject {
     @NSManaged var imageDescription: String?
     @NSManaged var location: String?
     @NSManaged var url: URL
+    @NSManaged var data: Data?
     @NSManaged var cache: ManagedCache
     
     static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
