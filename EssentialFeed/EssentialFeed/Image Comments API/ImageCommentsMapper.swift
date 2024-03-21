fd46a5fb616fa2dc38d029d710ad2d512c65fee2
Agnese Bussone <agnese.bussone@gmail.com>
2024-03-21 15:19:06 +0100

    Test ImageCommentsMapper in isolation

--- EssentialFeed/EssentialFeed/Image Comments API/ImageCommentsMapper.swift
+++ EssentialFeed/EssentialFeed/Image Comments API/ImageCommentsMapper.swift
@@ -2,7 +2,7 @@
 
 import Foundation
 
-internal final class ImageCommentsMapper {
+public final class ImageCommentsMapper {
     
     private struct Root: Decodable {
         private let items: [Item]
@@ -23,7 +23,7 @@ internal final class ImageCommentsMapper {
         }
     }
 
-    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [ImageComment] {
+    public static func map(_ data: Data, from response: HTTPURLResponse) throws -> [ImageComment] {
         let decoder = JSONDecoder()
         decoder.dateDecodingStrategy = .iso8601
         
