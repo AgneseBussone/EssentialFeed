41de4dfeedaba52c11bdf74de5c8127238a9da09
Agnese Bussone <agnese.bussone@gmail.com>
2024-03-25 15:37:39 +0100

    Add Image Comments title with localization

---
+++ EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsLocalizationTests.swift
@@ -0,0 +1,14 @@
+//
+
+import XCTest
+import EssentialFeed
+
+final class ImageCommentsLocalizationTests: XCTestCase {
+
+    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+        let table = "ImageComments"
+        let presentationBundle = Bundle(for: ImageCommentsPresenter.self)
+        assertLocalizedKeyAndValuesExist(in: presentationBundle, table)
+    }
+
+}
