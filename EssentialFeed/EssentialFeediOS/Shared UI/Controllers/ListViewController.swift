36159ee439d866b93edc66b983ec5d747a77078f
Agnese Bussone <agnese.bussone@gmail.com>
2024-03-28 14:27:04 +0100

    Recapture references and reloade the resource set free in the didEndDisplayingCell

--- EssentialFeed/EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
+++ EssentialFeed/EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
@@ -86,6 +86,11 @@ final public class ListViewController: UITableViewController{
         dl?.tableView?(tableView, didEndDisplaying: cell, forRowAt: indexPath)
     }
     
+    public override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+      let dl = cellController(at: indexPath)?.delegate
+      dl?.tableView?(tableView, willDisplay: cell, forRowAt: indexPath)
+    }
+    
     private func cellController(at indexPath: IndexPath) -> CellController? {
         dataSource.itemIdentifier(for: indexPath)
     }
